f3144733f8aa799cf84cabf8787a21a864c3743d
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-12-08 23:05:44 +0900

    로그인 성공시,localstorag에 token저장

--- src/API/API.js
+++ src/API/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const url = 'https://pre-onboarding-selection-task.shop/';
 
 export const api = {
-  signUp: ({ email, password }) => {
+  signup: ({ email, password }) => {
     axios.post("https://pre-onboarding-selection-task.shop/auth/signup", {
       "email": `${email}`,
       "password": `${password}`,
@@ -17,7 +17,7 @@ export const api = {
     }
     );
   },
-  logIn: ({ email, password }) => {
+  login: ({ email, password }) => {
     axios.post("https://pre-onboarding-selection-task.shop/auth/signin", {
       "email": `${email}`,
       "password": `${password}`,
@@ -26,7 +26,7 @@ export const api = {
         "Content-Type": "application/json",
       }
     }).then((res) => {
-      console.log(res)
+      localStorage.setItem('wtd_tk', res.data.access_token);
     }).catch((err) => {
       alert(JSON.parse(err.request.response).message); /* todo: id/pw 검증 분할 */
     })
