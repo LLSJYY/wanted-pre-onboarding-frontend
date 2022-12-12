a487ef259ac5ecc31357104f6e10cae48b1c0afd
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-12-12 21:34:10 +0900

    refactor : api 주석

--- src/API/API.js
+++ src/API/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const url = "https://pre-onboarding-selection-task.shop"
 export const api = {
+  // related to AUTH 
   signUp: ({ email, password }) => {
     return axios.post(`${url}/auth/signup`, {
       "email": `${email}`,
@@ -21,6 +22,8 @@ export const api = {
       }
     })
   },
+    // related to Todo
+
   initTodo: (accessToken) => {
     return axios.get(`${url}/todos`,
       {
@@ -48,30 +51,6 @@ export const api = {
     },
     )
   },
-  // completedTodo: (accessToken, item) => {
-  //   return axios.put(`https://pre-onboarding-selection-task.shop/todos/${item.id}`, {
-  //     todo: item.todo,
-  //     isCompleted: !item.isCompleted,
-  //   }, {
-  //     headers: {
-  //       "Authorization": `Bearer ${accessToken}`,
-  //       "Content-Type": "application/json",
-  //     }
-  //   })  
-  // },
-
-  // modifyTodo : (accessToken,{ item, newTodo }) => {
-  //   return axios.put(`https://pre-onboarding-selection-task.shop/todos/${item.id}`, {
-  //     todo: newTodo,
-  //     isCompleted: item.isCompleted,
-  //   }, {
-  //     headers: {
-  //       "Authorization": `Bearer ${accessToken}`,
-  //       "Content-Type": "application/json",
-  //     }
-  //   })
-  // },
-
   modifyTodo : (accessToken,{ item, newTodo }) => {
     return axios.put(`https://pre-onboarding-selection-task.shop/todos/${item.id}`, {
       todo: newTodo === undefined ? item.todo : newTodo,
