58e75f92ec4e082bd2c6b3fc62d2745630d2b4b4
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-12-15 17:27:29 +0900

    feat : newTodo 공백처리

--- src/Todo/NewTodo.js
+++ src/Todo/NewTodo.js
@@ -1,34 +1,34 @@
 import { useRef } from "react";
 
-const NewTodo = ({setNewTodo,onAddTodo}) => {
+const NewTodo = ({ setNewTodo, onAddTodo }) => {
   const inputRef = useRef();
   
-
-  const onKeyDownInput= (e)=>{
-
-    if(e.key === 'Enter'){
+  const onKeyDownInput = (e) => {
+    if (e.key === 'Enter' && inputRef.current.value !== '') {
       setNewTodo(inputRef.current.value);
       onAddTodo(inputRef.current.value);
       inputRef.current.value = "";
     }
   }
   const onClickBtn = () => {
-    setNewTodo(inputRef.current.value);
-    onAddTodo(inputRef.current.value);
-    inputRef.current.value = "";
+    if (inputRef.current.value !== '') {
+      setNewTodo(inputRef.current.value);
+      onAddTodo(inputRef.current.value);
+      inputRef.current.value = "";
+    }
   }
 
-  
+
   return (
     <div className="newTodo">
-    <input
-      onKeyDown={onKeyDownInput}
-      ref={inputRef}
-      placeholder="newTodo"
-/>
-    <button
-      onClick={onClickBtn}
-    >add</button>
+      <input
+        onKeyDown={onKeyDownInput}
+        ref={inputRef}
+        placeholder="newTodo"
+      />
+      <button
+        onClick={onClickBtn}
+      >add</button>
     </div>
   );
 }
